be27483c414da5e15bce9704291d8fb0f1729acb
dhruv0ps <dhruv.dekhtawala2002@gmail.com>
2025-04-24 14:06:16 +0530

    Test:Homepage

--- Frontend/src/features/Dashboard/Fleetenterpriseservices.tsx
+++ Frontend/src/features/Dashboard/Fleetenterpriseservices.tsx
@@ -12,19 +12,19 @@ const FleetEnterpriseServices: React.FC = () => {
 
           <div className="flex flex-row-reverse gap-x-6 items-center justify-between md:block mt-2 ">
 
-          <h2 className="text-3xl sm:text-2xl md:text-4xl font-bold text-primary text-right sm:text-left flex flex-col sm:flex-row sm:items-baseline sm:gap-4">
-  <span>Fleet</span>
-  <span className="text-lg sm:text-2xl md:text-4xl">Enterprise Services</span>
-</h2>
+            <h2 className="text-3xl sm:text-2xl md:text-4xl font-bold text-primary text-right sm:text-left flex flex-col sm:flex-row sm:items-baseline sm:gap-4">
+              <span>Fleet</span>
+              <span className="text-lg sm:text-2xl md:text-4xl">Enterprise Services</span>
+            </h2>
 
-<div className="bg-white rounded-xl shadow-lg w-30 h-28 md:w-32 md:h-32 flex items-center justify-center p-3 mt-0 md:mt-4">
-  <img
-    src={img1}
-    alt="Fleet Icon"
-    loading="lazy"
-    className="w-26 h-24"
-  />
-</div>
+            <div className="bg-white rounded-xl shadow-lg w-30 h-28 md:w-32 md:h-32 flex items-center justify-center p-3 mt-0 md:mt-4">
+              <img
+                src={img1}
+                alt="Fleet Icon"
+                loading="lazy"
+                className="w-26 h-24"
+              />
+            </div>
           </div>
 
           <div className="flex flex-col md:flex-row md:items-start gap-10 mt-6">
@@ -71,15 +71,13 @@ const FleetEnterpriseServices: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className="bg-[#e5f6ff]  ">
-      <div className="max-w-[80%] sm:max-w-[80%] mx-auto flex flex-col md:flex-row items-center justify-between gap-10 py-6 px-4 md:px-8 sm:py-16">
-          {/* Left Side */}
-          <div className="flex-1">
-            <div className="flex flex-row-reverse gap-x-16 sm:flex-col sm:items-start justify-between gap-4 mb-6">
-
-              <div>
-                <h2 className="text-2xl sm:text-4xl md:text-4xl font-bold text-primary text-right sm:text-left">
-
+      <div className="bg-[#e5f6ff]">
+      <div className="max-w-[90%] sm:max-w-[80%] mx-auto flex flex-col md:flex-row items-center justify-between gap-10 py-6 px-4 md:px-8 sm:py-16">
+        {/* Left Side */}
+        <div className="flex-1">
+          <div className="flex flex-row-reverse gap-x-8 sm:gap-x-16 sm:flex-col sm:items-start justify-between gap-4 mb-6">
+            <div className="max-w-[210px] sm:max-w-none">
+              <h2 className="text-2xl sm:text-4xl md:text-4xl font-bold text-primary text-right sm:text-left">
                   <span className="block sm:hidden">
                     <span className="block text-3xl">Overhauled</span>
                     <span className="block text-3xl">Competition</span>
@@ -96,7 +94,7 @@ const FleetEnterpriseServices: React.FC = () => {
                 <img
                   src={carImage}
                   alt="Competition Icon"
-                  className="w-24 h-24 object-contain"
+                    className="w-20 h-20 sm:w-24 sm:h-24 "
                 />
               </div>
             </div>
