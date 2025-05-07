38fd1406772d2ba345fe06a1ca2ec0e03c8cb257
dhruv0ps <dhruv.dekhtawala2002@gmail.com>
2025-05-07 07:17:35 +0530

    Fix :Turtle background

--- Frontend/src/features/Dashboard/TurtleSection.tsx
+++ Frontend/src/features/Dashboard/TurtleSection.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
-import turtleVideo from "@/assets/Homepage/iStock-464391536.mp4"; 
-import { ChevronRight } from "lucide-react";
-const TurtleSection: React.FC = () => {
+import React from "react"; 
+import { Button } from "@/components/ui/button"; 
+import turtleVideo from "@/assets/Homepage/iStock-464391536.mp4";  
+import { ChevronRight } from "lucide-react"; 
+
+const TurtleSection:React.FC = () => {
   return (
-    <div className="relative bg-black w-screen      ">
+    <div className="relative bg-black w-screen" style={{ WebkitBackgroundSize: "100% 100%", backgroundSize: "100% 100%" }}>
       <div className=" overflow-visible mb-12 min-h-[125px] py-8  mt-36 md:py-12 lg:mt-32 max-w-full sm:max-w-[80%] mx-auto ">
-      
         <div className="absolute -top-32 lg:-top-24 left-1/2 md:left-12 lg:left-24 transform -translate-x-1/2 md:translate-x-0 z-20 lg:mt-0 ">
           <div className="bg-white border border-white rounded-3xl md:gap-y-6 lg:ml-16  p-4 shadow-lg    mt-6 w-[400px] sm:w-[320px] md:w-[380px] lg:w-[400px]">
-          <h3 className="hidden md:block text-primary text-xl md:text-2xl font-bold text-end mb-2">
+            <h3 className="hidden md:block text-primary text-xl md:text-2xl font-bold text-end mb-2">
               Yep, that's me!
             </h3>
             <div className="w-full h-[200px] sm:h-[160px] md:h-[180px] lg:h-[200px]">
-            <video
+              <video
                 src={turtleVideo}
                 autoPlay
                 muted
@@ -24,27 +24,27 @@ const TurtleSection: React.FC = () => {
             </div>
           </div>
           <h3 className="block md:hidden text-primary text-2xl font-bold text-center mt-3">
-              Yep, that's me!
-            </h3>
+            Yep, that's me!
+          </h3>
         </div>
-
+        
         {/* Black Section Content */}
         <div className="flex flex-col md:flex-row items-center md:items-end justify-center md:justify-end gap-4 px-4 mt-32 md:mt-0">
           <p className="text-white text-center mt-6 sm:mt-0 md:text-right text-base md:text-xl font-semibold mb-4 md:mb-0">
             You're probably wondering how I ended up in this situation...
           </p>
           <Button className="group relative bg-primary hover:bg-sky-500 text-white font-semibold rounded-full px-4 md:px-6 py-2 shadow transition-all flex items-center justify-center overflow-hidden">
-  {/* Default Text */}
-  <span className="transition-opacity duration-200 group-hover:opacity-0">
-    Learn More
-  </span>
-
-  {/* Hover State with Arrow */}
-  <span className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none">
-    Learn More
-    <ChevronRight className=" h-5 w-5" />
-  </span>
-</Button>
+            {/* Default Text */}
+            <span className="transition-opacity duration-200 group-hover:opacity-0">
+              Learn More
+            </span>
+            
+            {/* Hover State with Arrow */}
+            <span className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none">
+              Learn More
+              <ChevronRight className=" h-5 w-5" />
+            </span>
+          </Button>
         </div>
       </div>
     </div>
