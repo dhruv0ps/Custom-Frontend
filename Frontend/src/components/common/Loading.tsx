1f18435f64fc184d3f7ca7fcd8d8ccfcdf1cb02d
dhruv0ps <dhruv.dekhtawala2002@gmail.com>
2025-05-15 12:56:42 +0530

    FIxed:vercel integartion testing

--- Frontend/src/components/common/Loading.tsx
+++ Frontend/src/components/common/Loading.tsx
@@ -6,7 +6,7 @@ interface LoadingProps {
 
 export const Loading = ({ full = false }: LoadingProps) => {
     return full ? <div className='flex h-screen items-center justify-center'><Loading /></div>
-        : (<div className="flex justify-center items-center overflow-hidden text-xl text-primary h-[500px]">
+        : (<div className="flex justify-center items-center overflow-hidden text-xl text-white h-[500px]">
             <Loader2Icon className="mr-2 h-9 w-9 animate-spin" /> Please wait
         </div>);
 }
